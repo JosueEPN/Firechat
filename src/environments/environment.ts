36c7f026727414e77f979f0d710672afc0912bc6
JosueEPN <65979995+JosueEPN@users.noreply.github.com>
2022-06-10 23:35:48 -0500

    Firechat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/quotes */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjkcx2wHnG3v6S0HUbXRlql_aLwwTl9oM",
+    authDomain: "proyectos-2022a-b2c09.firebaseapp.com",
+    databaseURL: "https://proyectos-2022a-b2c09-default-rtdb.firebaseio.com",
+    projectId: "proyectos-2022a-b2c09",
+    storageBucket: "proyectos-2022a-b2c09.appspot.com",
+    messagingSenderId: "1084324620160",
+    appId: "1:1084324620160:web:c13c96e0d469b70f918365",
+    measurementId: "G-64R2GJYXH6"
+  }
 };
 
 /*
